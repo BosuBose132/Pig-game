85ab097c6e66677ee029735a2595f8f8d4ee0616
Bosu <badebosubabu@gmail.com>
2025-12-29 18:06:33 -0500

    Reset the game using new game button

--- script.js
+++ script.js
@@ -11,13 +11,35 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  diceEl.classList.add('hidden');
+
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  scores[0] = 0;
+  scores[1] = 0;
+};
+
+init();
+
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
@@ -57,10 +79,10 @@ btnHold.addEventListener('click', function () {
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.remove('player--active');
+    } else {
+      switchPlayer();
     }
-    //3. if true, finish the game
-
-    //4. if false, switch to the next player
-    switchPlayer();
   }
 });
+
+btnNew.addEventListener('click', init);
